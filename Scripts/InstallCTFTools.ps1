006594018942a6d49eb93a6c0d509c6450341a4f
HankBoone <aaron.d.jones19@gmail.com>
2022-01-30 16:13:03 +0300

    Changed source of Installers

--- Scripts/InstallCTFTools.ps1
+++ Scripts/InstallCTFTools.ps1
@@ -29,7 +29,7 @@ $notify = new-object system.windows.forms.notifyicon
 $notify.icon = [System.Drawing.SystemIcons]::Information
 $notify.visible = $true
 $notify.showballoontip(10,'Burp Suite','Downloading Burp Suite Windows Version',[system.windows.forms.tooltipicon]::None)
-$source = 'https://github.com/HankBoone/HankBoone.github.io/tree/main/Installers/burpsuite_community_windows-x64_v2021_12_1.exe'
+$source = 'https://github.com/HankBoone/HankBoone.github.io/blob/main/Installers/burpsuite_community_windows-x64_v2021_12_1.exe'
 $destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\Installers\burpsuite_community_windows-x64_v2021_12_1.exe"
 Start-BitsTransfer -Source $source -Destination $destination
 CLEAR
@@ -41,7 +41,7 @@ $notify = new-object system.windows.forms.notifyicon
 $notify.icon = [System.Drawing.SystemIcons]::Information
 $notify.visible = $true
 $notify.showballoontip(10,'Metasploit','Downloading Metasploit Windows Version',[system.windows.forms.tooltipicon]::None)
-$source = 'https://github.com/HankBoone/HankBoone.github.io/tree/main/Installers/metasploitframework-latest.msi'
+$source = 'https://github.com/HankBoone/HankBoone.github.io/blob/main/Installers/metasploitframework-latest.msi'
 $destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\Installers\metasploitframework-latest.msi"
 Start-BitsTransfer -Source $source -Destination $destination
 CLEAR
@@ -53,7 +53,7 @@ $notify = new-object system.windows.forms.notifyicon
 $notify.icon = [System.Drawing.SystemIcons]::Information
 $notify.visible = $true
 $notify.showballoontip(10,'Wireshark','Downloading Wireshark Windows Edition',[system.windows.forms.tooltipicon]::None)
-$source = 'https://github.com/HankBoone/HankBoone.github.io/tree/main/Installers/Wireshark-win64-3.6.1.exe'
+$source = 'https://github.com/HankBoone/HankBoone.github.io/blob/main/Installers/Wireshark-win64-3.6.1.exe'
 $destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\Installers\Wireshark-win64-3.6.1.exe"
 Start-BitsTransfer -Source $source -Destination $destination
 CLEAR
@@ -65,7 +65,7 @@ $notify = new-object system.windows.forms.notifyicon
 $notify.icon = [System.Drawing.SystemIcons]::Information
 $notify.visible = $true
 $notify.showballoontip(10,'Nmap','Downloading Nmap Windows Edition',[system.windows.forms.tooltipicon]::None)
-$source = 'https://github.com/HankBoone/HankBoone.github.io/tree/main/Installers/nmap-7.92-setup.exe'
+$source = 'https://github.com/HankBoone/HankBoone.github.io/blob/main/Installers/nmap-7.92-setup.exe'
 $destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\Installers\nmap-7.92-setup.exe"
 Start-BitsTransfer -Source $source -Destination $destination
 CLEAR
