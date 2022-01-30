c604e1c6d322644cfacb231606897c09f527683b
HankBoone <aaron.d.jones19@gmail.com>
2022-01-30 20:06:40 +0300

    Changed download method

--- Scripts/InstallCTFTools.ps1
+++ Scripts/InstallCTFTools.ps1
@@ -10,63 +10,188 @@ CLEAR
 mkdir "C:\Users\$([Environment]::UserName)\Documents\CTFTools\VMs\"
 CLEAR
 
-<# Download VMWare Kali VM #>
-[reflection.assembly]::loadwithpartialname('System.Windows.Forms')
-[reflection.assembly]::loadwithpartialname('System.Drawing')
-$notify = new-object system.windows.forms.notifyicon
-$notify.icon = [System.Drawing.SystemIcons]::Information
-$notify.visible = $true
-$notify.showballoontip(10,'Kali VMWare','Downloading VMWare Kali VM',[system.windows.forms.tooltipicon]::None)
-$source = 'https://kali.download/virtual-images/kali-2021.4a/kali-linux-2021.4a-vmware-amd64.7z'
-$destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\VMs\kali-linux-2021.4a-vmware-amd64.7z"
-Start-BitsTransfer -Source $source -Destination $destination
-CLEAR
+     $title = "ChaosWraith CTF Tools Downloader"
+     $message = "Please select resources for download!"
+     $A = New-Object System.Management.Automation.Host.ChoiceDescription "&All", "All"
+     $B = New-Object System.Management.Automation.Host.ChoiceDescription "&Kali VMWare", "Download Kali VMWare"
+     $C = New-Object System.Management.Automation.Host.ChoiceDescription "&Burp Suite", "Download Burp Suite"
+     $D = New-Object System.Management.Automation.Host.ChoiceDescription "&Metasploit", "Download Metasploit"
+     $E = New-Object System.Management.Automation.Host.ChoiceDescription "&Wireshark", "Download Wireshark"
+     $F = New-Object System.Management.Automation.Host.ChoiceDescription "&Nmap", "Download Nmap"
+     $options = [System.Management.Automation.Host.ChoiceDescription[]]($A, $B, $C, $D, $E, $F)
+     $download=$host.ui.PromptForChoice($title, $message, $options, 0)
 
-<# Download Burp Suite Windows #>
-[reflection.assembly]::loadwithpartialname('System.Windows.Forms')
-[reflection.assembly]::loadwithpartialname('System.Drawing')
-$notify = new-object system.windows.forms.notifyicon
-$notify.icon = [System.Drawing.SystemIcons]::Information
-$notify.visible = $true
-$notify.showballoontip(10,'Burp Suite','Downloading Burp Suite Windows Version',[system.windows.forms.tooltipicon]::None)
-$source = 'https://github.com/HankBoone/HankBoone.github.io/blob/main/Installers/burpsuite_community_windows-x64_v2021_12_1.exe'
-$destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\Installers\burpsuite_community_windows-x64_v2021_12_1.exe"
-Start-BitsTransfer -Source $source -Destination $destination
-CLEAR
 
-<# Download Metasploit Windows #>
-[reflection.assembly]::loadwithpartialname('System.Windows.Forms')
-[reflection.assembly]::loadwithpartialname('System.Drawing')
-$notify = new-object system.windows.forms.notifyicon
-$notify.icon = [System.Drawing.SystemIcons]::Information
-$notify.visible = $true
-$notify.showballoontip(10,'Metasploit','Downloading Metasploit Windows Version',[system.windows.forms.tooltipicon]::None)
-$source = 'https://github.com/HankBoone/HankBoone.github.io/blob/main/Installers/metasploitframework-latest.msi'
-$destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\Installers\metasploitframework-latest.msi"
-Start-BitsTransfer -Source $source -Destination $destination
-CLEAR
 
-<# Download Wireshark Windows #>
-[reflection.assembly]::loadwithpartialname('System.Windows.Forms')
-[reflection.assembly]::loadwithpartialname('System.Drawing')
-$notify = new-object system.windows.forms.notifyicon
-$notify.icon = [System.Drawing.SystemIcons]::Information
-$notify.visible = $true
-$notify.showballoontip(10,'Wireshark','Downloading Wireshark Windows Edition',[system.windows.forms.tooltipicon]::None)
-$source = 'https://github.com/HankBoone/HankBoone.github.io/blob/main/Installers/Wireshark-win64-3.6.1.exe'
-$destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\Installers\Wireshark-win64-3.6.1.exe"
-Start-BitsTransfer -Source $source -Destination $destination
-CLEAR
+     switch ($download)
+		 {
+             0 {
+				$message = "Kali VMWare Downloaded!"
+                 <# Download VMWare Kali VM #>
+				[reflection.assembly]::loadwithpartialname('System.Windows.Forms')
+				[reflection.assembly]::loadwithpartialname('System.Drawing')
+				$notify = new-object system.windows.forms.notifyicon
+				$notify.icon = [System.Drawing.SystemIcons]::Information
+				$notify.visible = $true
+				$notify.showballoontip(10,'Kali VMWare','Downloading VMWare Kali VM',[system.windows.forms.tooltipicon]::None)
+				$source = 'https://kali.download/virtual-images/kali-2021.4a/kali-linux-2021.4a-vmware-amd64.7z'
+				$destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\VMs\kali-linux-2021.4a-vmware-amd64.7z"
+				$job = Start-BitsTransfer -Source $source -Destination $destination
+				While ($job.JobState -eq "Transferring" -Or $job.JobState -eq "Connecting") {
+    			Sleep -Seconds 20
+				}
 
-<# Download NMAP Windows #>
-[reflection.assembly]::loadwithpartialname('System.Windows.Forms')
-[reflection.assembly]::loadwithpartialname('System.Drawing')
-$notify = new-object system.windows.forms.notifyicon
-$notify.icon = [System.Drawing.SystemIcons]::Information
-$notify.visible = $true
-$notify.showballoontip(10,'Nmap','Downloading Nmap Windows Edition',[system.windows.forms.tooltipicon]::None)
-$source = 'https://github.com/HankBoone/HankBoone.github.io/blob/main/Installers/nmap-7.92-setup.exe'
-$destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\Installers\nmap-7.92-setup.exe"
-Start-BitsTransfer -Source $source -Destination $destination
-CLEAR
-ii "C:\Users\$([Environment]::UserName)\Documents\CTFTools"
+				If ($job.InternalErrorCode -ne 0) {
+    			("Error downloading the file {0}" -f $job.InternalErrorCode) | Out-File C:\downloads\downloaderror.log
+				} else {
+    			#Do something here
+    			#Stop-Computer -Force
+				}
+                Write-Output $message
+				CLEAR
+
+                $message = "Burp Suite Downloaded!"
+				<# Download Burp Suite Windows #>
+				[reflection.assembly]::loadwithpartialname('System.Windows.Forms')
+				[reflection.assembly]::loadwithpartialname('System.Drawing')
+				$notify = new-object system.windows.forms.notifyicon
+				$notify.icon = [System.Drawing.SystemIcons]::Information
+				$notify.visible = $true
+				$notify.showballoontip(10,'Burp Suite','Downloading Burp Suite Windows Version',[system.windows.forms.tooltipicon]::None)
+				$source = 'https://raw.githubusercontent.com/HankBoone/HankBoone.github.io/main/Installers/burpsuite_community_windows-x64_v2021_12_1.exe'
+				$destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\Installers\burpsuite_community_windows-x64_v2021_12_1.exe"
+				Invoke-WebRequest -Uri $source -OutFile $destination
+                Write-Output $message
+				CLEAR
+
+                $message = "Metasploit Downloaded!"
+				<# Download Metasploit Windows #>
+				[reflection.assembly]::loadwithpartialname('System.Windows.Forms')
+				[reflection.assembly]::loadwithpartialname('System.Drawing')
+				$notify = new-object system.windows.forms.notifyicon
+				$notify.icon = [System.Drawing.SystemIcons]::Information
+				$notify.visible = $true
+				$notify.showballoontip(10,'Metasploit','Downloading Metasploit Windows Version',[system.windows.forms.tooltipicon]::None)
+				$source = 'https://raw.githubusercontent.com/HankBoone/HankBoone.github.io/main/Installers/metasploitframework-latest.msi'
+				$destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\Installers\metasploitframework-latest.msi"
+				Invoke-WebRequest -Uri $source -OutFile $destination
+                Write-Output $message
+				CLEAR
+
+                $message = "Wireshark Downloaded!"
+				<# Download Wireshark Windows #>
+				[reflection.assembly]::loadwithpartialname('System.Windows.Forms')
+				[reflection.assembly]::loadwithpartialname('System.Drawing')
+				$notify = new-object system.windows.forms.notifyicon
+				$notify.icon = [System.Drawing.SystemIcons]::Information
+				$notify.visible = $true
+				$notify.showballoontip(10,'Wireshark','Downloading Wireshark Windows Edition',[system.windows.forms.tooltipicon]::None)
+				$source = 'https://raw.githubusercontent.com/HankBoone/HankBoone.github.io/main/Installers/Wireshark-win64-3.6.1.exe'
+				$destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\Installers\Wireshark-win64-3.6.1.exe"
+				Invoke-WebRequest -Uri $source -OutFile $destination
+                Write-Output $message
+				CLEAR
+
+                $message = "Nmap Downloaded!"
+				<# Download NMAP Windows #>
+				[reflection.assembly]::loadwithpartialname('System.Windows.Forms')
+				[reflection.assembly]::loadwithpartialname('System.Drawing')
+				$notify = new-object system.windows.forms.notifyicon
+				$notify.icon = [System.Drawing.SystemIcons]::Information
+				$notify.visible = $true
+				$notify.showballoontip(10,'Nmap','Downloading Nmap Windows Edition',[system.windows.forms.tooltipicon]::None)
+				$source = 'https://raw.githubusercontent.com/HankBoone/HankBoone.github.io/main/Installers/nmap-7.92-setup.exe'
+				$destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\Installers\nmap-7.92-setup.exe"
+				Invoke-WebRequest -Uri $source -OutFile $destination
+				CLEAR
+                Write-Output $message
+				ii "C:\Users\$([Environment]::UserName)\Documents\CTFTools"
+            }
+             1 {
+                $message = "Kali VMWare Downloaded!"
+				<# Download VMWare Kali VM #>
+				[reflection.assembly]::loadwithpartialname('System.Windows.Forms')
+				[reflection.assembly]::loadwithpartialname('System.Drawing')
+				$notify = new-object system.windows.forms.notifyicon
+				$notify.icon = [System.Drawing.SystemIcons]::Information
+				$notify.visible = $true
+				$notify.showballoontip(10,'Kali VMWare','Downloading VMWare Kali VM',[system.windows.forms.tooltipicon]::None)
+				$source = 'https://kali.download/virtual-images/kali-2021.4a/kali-linux-2021.4a-vmware-amd64.7z'
+				$destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\VMs\kali-linux-2021.4a-vmware-amd64.7z"
+				$job = Start-BitsTransfer -Source $source -Destination $destination
+				While ($job.JobState -eq "Transferring" -Or $job.JobState -eq "Connecting") {
+						Sleep -Seconds 20
+				}
+
+				If ($job.InternalErrorCode -ne 0) {
+						("Error downloading the file {0}" -f $job.InternalErrorCode) | Out-File C:\downloads\downloaderror.log
+				} else {
+						#Do something here
+						#Stop-Computer -Force
+				}
+				CLEAR
+				ii "C:\Users\$([Environment]::UserName)\Documents\CTFTools"
+             }
+            2 {
+                $message = "Burp Suite Downloaded!"
+				<# Download VMWare Kali VM #>
+				[reflection.assembly]::loadwithpartialname('System.Windows.Forms')
+				[reflection.assembly]::loadwithpartialname('System.Drawing')
+				$notify = new-object system.windows.forms.notifyicon
+				$notify.icon = [System.Drawing.SystemIcons]::Information
+				$notify.visible = $true
+				$notify.showballoontip(10,'Burp Suite','Downloading Burp Suite Windows Version',[system.windows.forms.tooltipicon]::None)
+				$source = 'https://raw.githubusercontent.com/HankBoone/HankBoone.github.io/main/Installers/burpsuite_community_windows-x64_v2021_12_1.exe'
+				$destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\Installers\burpsuite_community_windows-x64_v2021_12_1.exe"
+				Invoke-WebRequest -Uri $source -OutFile $destination
+				CLEAR
+				ii "C:\Users\$([Environment]::UserName)\Documents\CTFTools"
+            }
+            3 {
+                $message = "Metasploit Downloaded!"
+                <# Download Metasploit Windows #>
+				[reflection.assembly]::loadwithpartialname('System.Windows.Forms')
+				[reflection.assembly]::loadwithpartialname('System.Drawing')
+				$notify = new-object system.windows.forms.notifyicon
+				$notify.icon = [System.Drawing.SystemIcons]::Information
+				$notify.visible = $true
+				$notify.showballoontip(10,'Metasploit','Downloading Metasploit Windows Version',[system.windows.forms.tooltipicon]::None)
+				$source = 'https://raw.githubusercontent.com/HankBoone/HankBoone.github.io/main/Installers/metasploitframework-latest.msi'
+				$destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\Installers\metasploitframework-latest.msi"
+				Invoke-WebRequest -Uri $source -OutFile $destination
+				CLEAR
+				ii "C:\Users\$([Environment]::UserName)\Documents\CTFTools"
+            }
+            4 {
+                $message = "Wireshark Downloaded!"
+                <# Download Wireshark Windows #>
+				[reflection.assembly]::loadwithpartialname('System.Windows.Forms')
+				[reflection.assembly]::loadwithpartialname('System.Drawing')
+				$notify = new-object system.windows.forms.notifyicon
+				$notify.icon = [System.Drawing.SystemIcons]::Information
+				$notify.visible = $true
+				$notify.showballoontip(10,'Wireshark','Downloading Wireshark Windows Edition',[system.windows.forms.tooltipicon]::None)
+				$source = 'https://raw.githubusercontent.com/HankBoone/HankBoone.github.io/main/Installers/Wireshark-win64-3.6.1.exe'
+				$destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\Installers\Wireshark-win64-3.6.1.exe"
+				Invoke-WebRequest -Uri $source -OutFile $destination
+				CLEAR
+				ii "C:\Users\$([Environment]::UserName)\Documents\CTFTools"
+            }
+            5 {
+                $message = "Nmap Downloaded!"
+                <# Download NMAP Windows #>
+				[reflection.assembly]::loadwithpartialname('System.Windows.Forms')
+				[reflection.assembly]::loadwithpartialname('System.Drawing')
+				$notify = new-object system.windows.forms.notifyicon
+				$notify.icon = [System.Drawing.SystemIcons]::Information
+				$notify.visible = $true
+				$notify.showballoontip(10,'Nmap','Downloading Nmap Windows Edition',[system.windows.forms.tooltipicon]::None)
+				$source = 'https://raw.githubusercontent.com/HankBoone/HankBoone.github.io/main/Installers/nmap-7.92-setup.exe'
+				$destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\Installers\nmap-7.92-setup.exe"
+				Invoke-WebRequest -Uri $source -OutFile $destination
+				CLEAR
+				ii "C:\Users\$([Environment]::UserName)\Documents\CTFTools"
+            }
+         }
+		 Write-Output $message
+  
