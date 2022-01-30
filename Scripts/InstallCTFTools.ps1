854c6132400be384df3db2a4bc33381475df5971
HankBoone <aaron.d.jones19@gmail.com>
2022-01-30 16:53:09 +0300

    Uncommented Kali VM

--- Scripts/InstallCTFTools.ps1
+++ Scripts/InstallCTFTools.ps1
@@ -11,16 +11,16 @@ mkdir "C:\Users\$([Environment]::UserName)\Documents\CTFTools\VMs\"
 CLEAR
 
 <# Download VMWare Kali VM #>
-<# [reflection.assembly]::loadwithpartialname('System.Windows.Forms')
+[reflection.assembly]::loadwithpartialname('System.Windows.Forms')
 [reflection.assembly]::loadwithpartialname('System.Drawing')
 $notify = new-object system.windows.forms.notifyicon
 $notify.icon = [System.Drawing.SystemIcons]::Information
 $notify.visible = $true
 $notify.showballoontip(10,'Kali VMWare','Downloading VMWare Kali VM',[system.windows.forms.tooltipicon]::None)
-$source = 'https://github.com/HankBoone/HankBoone.github.io/tree/main/VMs/kali-linux-2021.4a-vmware-amd64.7z'
+$source = 'https://kali.download/virtual-images/kali-2021.4a/kali-linux-2021.4a-vmware-amd64.7z'
 $destination = "C:\Users\$([Environment]::UserName)\Documents\CTFTools\VMs\kali-linux-2021.4a-vmware-amd64.7z"
 Start-BitsTransfer -Source $source -Destination $destination
-CLEAR #>
+CLEAR
 
 <# Download Burp Suite Windows #>
 [reflection.assembly]::loadwithpartialname('System.Windows.Forms')
